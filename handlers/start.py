65dc07c4e48bd119ae87809b58885b4fbc85569f
Saivel1 <saivel.mezencev1@gmail.com>
2025-10-28 14:33:56 +0300

    pay menu

--- handlers/start.py
+++ handlers/start.py
@@ -1,8 +1,18 @@
-from aiogram import types
+from aiogram import F, types
 from aiogram.filters import Command
 from bot_instance import dp
-from keyboards.gen_menu import MainKeyboard
+from keyboards.markup import MainKeyboard
+from keyboards.builder import PayMenu
+from aiogram.types import CallbackQuery
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer(f"Привет! Твой ID: {message.from_user.id}", reply_markup=MainKeyboard.main_keyboard())
+    await message.answer(f"Привет! Твой ID: {message.from_user.id}", reply_markup=MainKeyboard.main_keyboard()) # type: ignore
+
+
+@dp.callback_query(F.data == "pay_menu")
+async def pay_menu(callback: CallbackQuery):
+    await callback.message.edit_text(
+        text="Меню покупки",
+        reply_markup=PayMenu.main_keyboard()
+    )
