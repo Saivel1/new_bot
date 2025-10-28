a2a6a8697288834866bc26b283401cdfe1230496
Saivel1 <saivel.mezencev1@gmail.com>
2025-10-28 14:52:37 +0300

    all menus

--- handlers/start.py
+++ handlers/start.py
@@ -2,16 +2,17 @@ from aiogram import F, types
 from aiogram.filters import Command
 from bot_instance import dp
 from keyboards.markup import MainKeyboard
-from keyboards.builder import PayMenu, SubMenu
+from keyboards.builder import PayMenu, SubMenu, InstructionMenu
 from aiogram.types import CallbackQuery
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer(f"Привет! Твой ID: {message.from_user.id}", reply_markup=MainKeyboard.main_keyboard()) # type: ignore
 
+
 @dp.callback_query(F.data == "start_menu")
 async def call_start(callback: CallbackQuery):
-    await callback.message.edit_text(f"Привет! Твой ID: {message.from_user.id}", reply_markup=MainKeyboard.main_keyboard()) # type: ignore
+    await callback.message.edit_text(f"Привет! Твой ID: {callback.from_user.id}", reply_markup=MainKeyboard.main_keyboard()) # type: ignore
 
 
 @dp.callback_query(F.data == "pay_menu")
@@ -21,9 +22,18 @@ async def pay_menu(callback: CallbackQuery):
         reply_markup=PayMenu.main_keyboard()
     )
 
-@dp.callback_query(F.data == "pay_menu")
+
+@dp.callback_query(F.data == "subs")
 async def subs_menu(callback: CallbackQuery):
     await callback.message.edit_text( #type:ignore
         text="Меню подписок",
         reply_markup=SubMenu.main_keyboard()
+    )
+
+
+@dp.callback_query(F.data == "instruction")
+async def instriction_menu(callback: CallbackQuery):
+    await callback.message.edit_text( #type:ignore
+        text="Меню инструкций",
+        reply_markup=InstructionMenu.main_keyboard()
     )
