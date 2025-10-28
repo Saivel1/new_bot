09fd12d410b1597df7f028e1b873a8b312f3439e
Saivel1 <saivel.mezencev1@gmail.com>
2025-10-28 14:23:17 +0300

    main menu

--- handlers/start.py
+++ handlers/start.py
@@ -1,7 +1,8 @@
 from aiogram import types
 from aiogram.filters import Command
 from bot_instance import dp
+from keyboards.gen_menu import MainKeyboard
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer(f"Привет! Твой ID: {message.from_user.id}")
+    await message.answer(f"Привет! Твой ID: {message.from_user.id}", reply_markup=MainKeyboard.main_keyboard())
---
+++ keyboards/gen_menu.py
@@ -0,0 +1,12 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+class MainKeyboard:
+    
+    @staticmethod
+    def main_keyboard():
+
+        return InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="Оплатить", callback_data="pay_menu")],
+            [InlineKeyboardButton(text="Подписка и ссылки", callback_data="subs")],
+            [InlineKeyboardButton(text="Инструкция", callback_data="instruction")]
+        ])
