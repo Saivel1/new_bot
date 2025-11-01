67b2d5329a61fc791b1d7be747482ef5752dab98
Saivel1 <saivel.mezencev1@gmail.com>
2025-11-01 22:31:22 +0300

    fkldsfs

--- keyboards/markup.py
+++ keyboards/markup.py
@@ -14,7 +14,7 @@ class MainKeyboard:
     @staticmethod
     def main_keyboard_with_trial():
         return InlineKeyboardMarkup(inline_keyboard=[
-            [InlineKeyboardButton(text="🎁 Пробный период", callback_data="trial")],
+            # [InlineKeyboardButton(text="🎁 Пробный период", callback_data="trial")],
             [InlineKeyboardButton(text="💳 Оплатить", callback_data="pay_menu")],
             [InlineKeyboardButton(text="🔗 Подписка и ссылки", callback_data="subs")],
             [InlineKeyboardButton(text="📱 Инструкция", callback_data="instruction")]
